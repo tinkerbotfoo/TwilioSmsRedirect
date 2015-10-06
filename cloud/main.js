ebc4daa564136760cc7594f154a9972f5099937c
PM <PM>
2015-10-06 00:55:09 -0700

    Added parse project files & added a test function to the main.js

--- cloud/main.js
+++ cloud/main.js
@@ -11,8 +11,14 @@ var app = express();
 app.use(express.json());
 app.use(express.bodyParser());
 
+app.get('/es', function (request, response) {
+    console.log("Entering Application - watta karvad ...");
+    response.send("super duper 2")
+});
+
 app.post('/endpoint', function (request, response) {
     "use strict";
+    console.log("Entering Application");
     var twiml, smsBody, smsTo, smsFrom;
 
     response.type('text/xml');
@@ -27,7 +33,7 @@ app.post('/endpoint', function (request, response) {
     twilio.sendSMS({
         From: smsTo,
         To: phoneNumber,
-        Body: "From " + smsFrom + ": " + smsBody // we're attaching a "From <number>:" here.
+        Body: "Twilio " + smsFrom + " to " + smsTo + ": " + smsBody // we're attaching a "From <number>:" here.
     }, {
         success: function () {
             console.log("Sent SMS successfully");
